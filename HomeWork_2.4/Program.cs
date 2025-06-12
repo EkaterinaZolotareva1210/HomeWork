73d7ef28950069ff766b4ede506ff0212418e06c
Екатерина Золотарева <pmkate1210@gmail.com>
2025-06-12 11:23:52 +0700

    fix

---
+++ HomeWork_2.4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите четырехзначное число: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int a = n / 1000;
+            int b = (n/100) % 10;
+            int c = (n/10) % 10;
+            int d = n % 10;
+            int n2 = a * 1000 + d * 100 + c * 10 + b;
+            Console.WriteLine(n2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWork_2.5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Через запятую введите длину трубы в метрах: ");
+            double length = Convert.ToDouble(Console.ReadLine());
+            double length_m = Math.Ceiling(length);
+            Console.WriteLine(length_m);
+            Console.ReadKey();
+        }
+    }
+}
